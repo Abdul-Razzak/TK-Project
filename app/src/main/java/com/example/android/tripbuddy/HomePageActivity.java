43e85f0a5d6a7520cb75ed23947d5e998a205887
Abdul-Razzak <abd.razzak.shaikh@gmail.com>
2017-06-02 23:37:56 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/android/tripbuddy/HomePageActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.tripbuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+
+    public void openSignInPage(View view)   {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+
+    public void openSignUpPage(View view)   {
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+    }
+}
